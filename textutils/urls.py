6db0af2cb54b4b61c22b038434e3d61cc9ae8c56
shivhub09 <shivnagori2020@gmail.com>
2023-07-07 20:22:11 +0530

    front page is created in django file

--- textutils/urls.py
+++ textutils/urls.py
@@ -20,11 +20,10 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('removepunc', views.removepunc, name='rempun'),
     path('capitalizefirst', views.capfirst, name='capfirst'),
-    path('removeline', views.lineremove, name='lineremove'),
+    path('newlineremove', views.newlineremove, name='newlineremove'),
     path('spaceremove', views.spaceremove, name='spaceremove'),
     path('charcount', views.charcount, name='charcount'),
-    path('removepunc', views.removepunc, name='removepunc'),
-
 
 ]
--- textutils/views.py
+++ textutils/views.py
@@ -1,23 +1,29 @@
-# i have created this file - Shivam
+# I have created this file - Harry
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    params = {'name':'harry', 'place':"Mars"}
-    return render(request, 'index.html', params)
+    return render(request, 'index.html')
+
+    # return HttpResponse("Home")
+
+def removepunc(request):
+    # get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    # analyze the text
+    return HttpResponse("removepunc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("newline remove first")
 
 
-def removepunc():
-    return HttpResponse("remove punc")
+def spaceremove(request):
+    return HttpResponse("space remover back")
 
-def capfirst():
-    return HttpResponse("capitalize")
-
-def lineremove():
-    return HttpResponse("remove an extra line   ")
-
-def spaceremove():
-    return HttpResponse("remove an space   ")
-
-def charcount():
-    return HttpResponse("count the number of characters   ")
+def charcount(request):
+    return HttpResponse("charcount ")
